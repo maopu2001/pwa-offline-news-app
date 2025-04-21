5cdaf63d981c1982819f59858f5e39c9fbf5966a
maopu2001 <maopu2001@gmail.com>
2025-04-21 23:53:44 +0600

    fixed connection status using navigator

--- src/hooks/useConnectionStatus.ts
+++ src/hooks/useConnectionStatus.ts
@@ -1,27 +1,19 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function useInternetStatus(url = 'https://httpstat.us/200', interval = 1000) {
+function useInternetStatus(interval = 1000) {
   const [isOnline, setIsOnline] = useState(false);
 
   useEffect(() => {
-    const checkInternet = async () => {
-      const pingUrl = url + '?q=' + Math.random() * 1000000;
-      try {
-        await axios.get(pingUrl);
-        setIsOnline(true);
-      } catch (error) {
-        setIsOnline(false);
-      }
+    const checkInternet = () => {
+      if (navigator) setIsOnline(navigator.onLine);
     };
 
     checkInternet();
     const id = setInterval(checkInternet, interval);
 
     return () => clearInterval(id);
-  }, [url, interval]);
+  }, [interval]);
 
-  console.log(isOnline);
   return isOnline;
 }
 
